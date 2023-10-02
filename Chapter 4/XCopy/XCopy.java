41559284668555cbcc7f6b40cb132a44995263c4
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 22:02:14 -0400

    Create XCopy.java

---
+++ Chapter 4/XCopy/XCopy.java
@@ -0,0 +1,17 @@
+
+public class XCopy {
+
+    private int orig;
+
+    public void setOrig(int o) {
+        orig = o;
+    }
+    public int getOrig() {
+        return orig;
+    }
+
+    int go(int arg) {
+        arg = arg * 2;
+        return arg;
+    }
+}
