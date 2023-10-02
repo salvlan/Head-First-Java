6e97370852a6f27a7c776c4584ac800472303ed8
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 21:53:39 -0400

    Update and rename Chapter 2/TapeDeck.java to Chapter 2/TapeDeck/TapeDeckTestDrive.java

--- Chapter 2/TapeDeck/TapeDeckTestDrive.java
+++ Chapter 2/TapeDeck/TapeDeckTestDrive.java
@@ -10,16 +10,3 @@ public class TapeDeckTestDrive {
         }
     }
 }
-
-public class TapeDeck {
-
-    boolean canRecord = false;
-
-    void playTape() {
-        System.out.println("tape playng");
-    }
-
-    void recordTape() {
-        System.out.println("tape recording");
-    }
-}
