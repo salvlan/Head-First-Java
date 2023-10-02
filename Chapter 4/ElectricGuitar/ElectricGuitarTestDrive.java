2b196c12069f1a09839d3a3d5eaad53149266563
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 21:58:43 -0400

    Update and rename Chapter 4/ElectricGuitar.java to Chapter 4/ElectricGuitar/ElectricGuitarTestDrive.java

---
+++ Chapter 4/ElectricGuitar/ElectricGuitarTestDrive.java
@@ -0,0 +1,11 @@
+public class ElectricGuitarTestDrive {
+    public static void main(String[] args) {
+
+        ElectricGuitar thePower = new ElectricGuitar();
+
+        thePower.setBrand("Yamaha");
+        thePower.getBrand();
+        thePower.setNumOfPickups(6);
+        thePower.getNumOfPickups();
+    }
+}
