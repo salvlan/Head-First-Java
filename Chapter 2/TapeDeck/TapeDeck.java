da9974ff0af308390110d2a6c4bce93a70f513f6
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 21:53:53 -0400

    Create TapeDeck.java

---
+++ Chapter 2/TapeDeck/TapeDeck.java
@@ -0,0 +1,13 @@
+
+public class TapeDeck {
+
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("tape playng");
+    }
+
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
