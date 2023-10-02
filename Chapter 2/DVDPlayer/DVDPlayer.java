b8dca2f08568ec26b72bd4ff9a4b482180c79ef9
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 21:44:38 -0400

    Update and rename Chapter 2/DVDPlayer.java to Chapter 2/DVDPlayer/DVDPlayer.java

---
+++ Chapter 2/DVDPlayer/DVDPlayer.java
@@ -0,0 +1,12 @@
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void playDVD() {
+        System.out.println("DVD playing");
+    }
+
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+}
