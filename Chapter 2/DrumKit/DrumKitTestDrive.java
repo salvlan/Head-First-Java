e8bd89ce29788aeb4c8e5b6590a2f89f25591b92
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 21:48:08 -0400

    Update and rename Chapter 2/DrumKit.java to Chapter 2/DrumKit/DrumKitTestDrive.java

--- Chapter 2/DrumKit/DrumKitTestDrive.java
+++ Chapter 2/DrumKit/DrumKitTestDrive.java
@@ -11,16 +11,3 @@ public class DrumKitTestDrive {
         }
     }
 }
-
-public class DrumKit {
-    boolean topHat = true;
-    boolean snare = true;
-
-    void playSnare() {
-        System.out.println("bang bang ba-bang");
-    }
-
-    void playTopHat() {
-        System.out.println("ding ding da-ding");
-    }
-}
