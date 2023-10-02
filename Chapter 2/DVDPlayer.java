762360e259ca063b683ca5a4c733057d107e1107
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 20:40:57 -0400

    Create DVDPlayer.java

---
+++ Chapter 2/DVDPlayer.java
@@ -0,0 +1,25 @@
+public class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+
+        if (d.canRecord == true) {
+            d.recordDVD();
+        }
+    }
+}
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void playDVD() {
+        System.out.println("DVD playing");
+    }
+
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+}
