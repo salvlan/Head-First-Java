7e78b5c4b8d0ddb734a0d4449bb2cfb5414d4b6c
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-01 22:01:01 -0400

    Update and rename Chapter 4/Puzzle4.java to Chapter 4/Puzzle4/Puzzle4.java

--- Chapter 4/Puzzle4/Puzzle4.java
+++ Chapter 4/Puzzle4/Puzzle4.java
@@ -26,16 +26,3 @@ public class Puzzle4 {
         System.out.println("Result " + result);
     }
 }
-
-public class Puzzle4b {
-
-    int ivar;
-
-    public int doStuff(int factor) {
-        if (ivar > 100) {
-            return ivar * factor;
-        } else {
-            return ivar * (5 - factor);
-        }
-    }
-}
