d216cf19c8f0b3604d5070dfe5dd4173e2137ca7
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-05 22:18:52 -0400

    Add files via upload

---
+++ Chapter 11/MusicTest1.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+
+    public void play () {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+        } catch (MidiUnavailableException ex) {
+            System.out.println ("Bummer");
+        }
+        System.out.println("We got a sequencer");
+    }
+
+    public static void main (String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
