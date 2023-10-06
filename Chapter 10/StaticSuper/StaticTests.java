77917275f6a5287ed703864e9b4e55f517ab07c7
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-05 22:17:34 -0400

    Add files via upload

---
+++ Chapter 10/StaticSuper/StaticTests.java
@@ -0,0 +1,15 @@
+public class StaticTests extends StaticSuper{
+    static int rand;
+    static {
+        rand = (int) (Math.random() * 6);
+        System.out.println ("static block " + rand);
+    }
+    StaticTests () {
+        System.out.println ("costuctor");
+    }
+
+    public static void main(String[] args) {
+    System.out.println ("in main");
+    StaticTests st = new StaticTests();
+    }
+}
