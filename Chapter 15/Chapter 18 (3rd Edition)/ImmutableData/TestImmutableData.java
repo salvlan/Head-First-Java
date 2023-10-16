6dc417b073eb7ae54157c1602c814b12cc7d0fb3
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-15 20:04:41 -0400

    Add files via upload

---
+++ Chapter 15/Chapter 18 (3rd Edition)/ImmutableData/TestImmutableData.java
@@ -0,0 +1,8 @@
+public class TestImmutableData {
+    public static void main(String[] args) {
+        ImmutableData iT = new ImmutableData("Rex", 5);
+
+        System.out.println(iT.getName() + " " + iT.getValue());
+
+    }
+}
