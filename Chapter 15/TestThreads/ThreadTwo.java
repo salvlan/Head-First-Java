e587340d93750c7011fc63d098b79cde91cb1e09
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-16 19:02:28 -0400

    Add files via upload

---
+++ Chapter 15/TestThreads/ThreadTwo.java
@@ -0,0 +1,21 @@
+public class ThreadTwo implements Runnable {
+
+    Accum a = Accum.getAccum();
+
+    public void run(){
+
+        for(int x=0; x<99; x++){
+
+            a.updateCounter(1);
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) { }
+
+        }
+
+        System.out.println("two " + a.getCount());
+
+    }
+
+}
