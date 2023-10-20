e0cdfbb2cbf3782103773a58e34e2e495208997e
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-20 14:42:01 -0400

    Create Jukebox1.java

---
+++ Chapter 16/Chapter 11 (3rd Edition)/Jukebox1/Jukebox1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Jukebox1 {
+
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go(){
+        List<String> songList = MockSongs.getSongString();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+}
