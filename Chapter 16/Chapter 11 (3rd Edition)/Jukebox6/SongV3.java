d957d295cec0eea2504364d84336adcce15537d7
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-20 14:45:52 -0400

    Add files via upload

---
+++ Chapter 16/Chapter 11 (3rd Edition)/Jukebox6/SongV3.java
@@ -0,0 +1,29 @@
+public class SongV3 {
+
+    private String title;
+    private String artist;
+    private int bpm;
+
+    SongV3 (String title, String artist, int bpm){
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public String getTitle (){
+        return title;
+    }
+
+    public String getArtist (){
+        return artist;
+    }
+
+    public int getBpm (){
+        return bpm;
+    }
+
+    public String toString(){
+        return title;
+    }
+
+}
