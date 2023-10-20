cf0feaf479d958cc8e271955fc3cbf95c074b031
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-20 14:42:13 -0400

    Add files via upload

---
+++ Chapter 16/Chapter 11 (3rd Edition)/Jukebox1/MockSongs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MockSongs {
+
+    public static List<String> getSongString(){
+
+        List <String> songs = new ArrayList<>();
+
+        songs.add("somersault");
+        songs.add("cassidy");
+        songs.add("$10");
+        songs.add("havana");
+        songs.add("Cassidy");
+        songs.add("50 Ways");
+
+        return songs;
+
+    }
+}
