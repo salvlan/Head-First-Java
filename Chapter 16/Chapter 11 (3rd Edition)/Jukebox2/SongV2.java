f2f68d46c05aeae825057a128b2dce2f6bc8d446
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-20 14:43:37 -0400

    Add files via upload

---
+++ Chapter 16/Chapter 11 (3rd Edition)/Jukebox2/SongV2.java
@@ -0,0 +1,33 @@
+public class SongV2 implements Comparable<SongV2> {
+
+    private String title;
+    private String artist;
+    private int bpm;
+
+    SongV2 (String title, String artist, int bpm){
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public int compareTo (SongV2 s){
+        return title.compareTo(s.getTitle());
+    }
+
+    public String getTitle (){
+        return title;
+    }
+
+    public String getArtist (){
+        return artist;
+    }
+
+    public int getBpm (){
+        return bpm;
+    }
+
+    public String toString(){
+        return title;
+    }
+
+}
